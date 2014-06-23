dc8a70d6c6ce82419c7fd8266a7f7c4068a0aa70
Goldman-Kirst <goldmae@cobalt.com>
2014-06-23 08:48:30 -0700

    cleaned up test class

--- src/test/java/ut/com/teamepic/plugin/test/MyPluginServletTest.java
+++ src/test/java/ut/com/teamepic/plugin/test/MyPluginServletTest.java
@@ -2,16 +2,24 @@ package ut.com.teamepic.plugin.test;
 
 import static org.junit.Assert.*;
 
+import javax.servlet.http.HttpServlet;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import com.teamepic.plugin.MyPluginServlet;
 
 public class MyPluginServletTest {
 	
+	HttpServlet servlet;
+	
+	@Before
+	public void setup() {
+		servlet = new MyPluginServlet(null, null, null);
+	}
+	
 	@Test
 	public void testConstructorNotNull() {
-		MyPluginServlet servlet = new MyPluginServlet(null, null, null);
 		assertNotNull("Servlet is null", servlet);
 	}
-
 }

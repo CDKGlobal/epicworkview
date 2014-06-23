c49dea2f98b3a151e0c1454e91eb643f9f4bd0d9
Goldman-Kirst <goldmae@cobalt.com>
2014-06-23 08:36:12 -0700

    added eclipse configuration and unit test class

---
+++ src/test/java/ut/com/teamepic/plugin/test/MyPluginServletTest.java
@@ -0,0 +1,16 @@
+package ut.com.teamepic.plugin.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.teamepic.plugin.MyPluginServlet;
+
+public class MyPluginServletTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}

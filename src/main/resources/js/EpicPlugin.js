819e220419182516fe85f679010c4a527783ac2a
Goldman-Kirst <goldmae@cobalt.com>
2014-06-24 15:27:35 -0700

    refresh projects every ten seconds

--- src/main/resources/js/EpicPlugin.js
+++ src/main/resources/js/EpicPlugin.js
@@ -1,10 +1,24 @@
-function ProjectController($scope, $http){
-	$http.get('/jira/rest/epic/1/projects.json').
-    success(function(data, status, headers, config) {
-      $scope.projects = data.projects;
-    }).
-    error(function(data, status, headers, config) {
-      // log error
-    });
+// Controller to manage table of projects
+
+function ProjectController($scope, $http) {
+
+	
+       
+    // Get all the projects and set them in a local variable (projects)
+    $scope.getProjects = function() {
+    	$http.get('/jira/rest/epic/1/projects.json').
+	    success(function(data, status, headers, config) {
+	      $scope.projects = data.projects;
+	    }).
+	    error(function(data, status, headers, config) {
+	      // log error
+	    });
+    }
+    
+    // Get the projects now
+    $scope.getProjects();
+    
+    // Get projects again every 10 seconds
+    setInterval(function(){$scope.getProjects();}, 10000);
 }
 	

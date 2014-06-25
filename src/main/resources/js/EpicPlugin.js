b00ba0c8653cb16387b03c6e26d426e4ce0fb909
Goldman-Kirst <goldmae@cobalt.com>
2014-06-25 08:46:19 -0700

    added toggle for epic descriptions

--- src/main/resources/js/EpicPlugin.js
+++ src/main/resources/js/EpicPlugin.js
@@ -1,8 +1,7 @@
-// Controller to manage table of projects
-
+/*
+ * Controller to manage table of projects
+ */
 function ProjectController($scope, $http) {
-
-	
        
     // Get all the projects and set them in a local variable (projects)
     $scope.getProjects = function() {
@@ -20,5 +19,28 @@ function ProjectController($scope, $http) {
     
     // Get projects again every 10 seconds
     setInterval(function(){$scope.getProjects();}, 10000);
+    
+}
+
+/*
+ * Controller for the epics of a project
+ * Determines which epic information to display
+ */
+function EpicController($scope) {
+	$scope.clickedEpic = null;
+    
+    // Set the clicked epic to be name or null if it is already name
+    $scope.toggleEpic = function(name) {
+    	if ($scope.clickedEpic == name) {
+    		$scope.clickedEpic = null;
+    	} else {
+    		$scope.clickedEpic = name;
+    	}
+    }
+    
+    // Return whether the clicked epic is this epic
+    $scope.showEpicDescription = function(name) {
+    	return $scope.clickedEpic == name;
+    }
 }
 	

89e7c99b49ef6a5034d6810d2a8efd9ad1d5a5ea
Justin Emge <emgej@cobalt.com>
2014-06-26 21:54:11 +0000

    fixed formatting

--- src/main/java/com/teamepic/plugin/MyPluginServlet.java
+++ src/main/java/com/teamepic/plugin/MyPluginServlet.java
@@ -1,51 +1,53 @@
 package com.teamepic.plugin;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.net.URI;
 import com.atlassian.sal.api.auth.LoginUriProvider;
 import com.atlassian.sal.api.user.UserKey;
 import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.templaterenderer.TemplateRenderer;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+
+
 public class MyPluginServlet extends HttpServlet {
-	
-	private final UserManager userManager;
-	private final LoginUriProvider loginUriProvider;
-	private final TemplateRenderer templateRenderer;
-	
-	public MyPluginServlet(UserManager userManager,
-			LoginUriProvider loginUriProvider,
-			TemplateRenderer templateRenderer) {
-		this.userManager = userManager;
-		this.loginUriProvider = loginUriProvider;
-		this.templateRenderer = templateRenderer;
-	}
-	
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		UserKey userKey = userManager.getRemoteUserKey(req);
-		if (userKey == null || !userManager.isSystemAdmin(userKey)) {
-			redirectToLogin(req, resp);
-			return;
-		}
-		templateRenderer.render("epic.vm", resp.getWriter());
-	}
-	
-	private void redirectToLogin(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.sendRedirect(loginUriProvider.getLoginUri(getUri(req)).toASCIIString());
-	}
-	
-	private URI getUri(HttpServletRequest req) {
-		StringBuffer builder = req.getRequestURL();
-		if (req.getQueryString() != null) {
-			builder.append("?");
-			builder.append(req.getQueryString());
-		}
-		return URI.create(builder.toString());
-	}
-	
+
+    private final UserManager userManager;
+    private final LoginUriProvider loginUriProvider;
+    private final TemplateRenderer templateRenderer;
+
+    public MyPluginServlet(UserManager userManager,
+                           LoginUriProvider loginUriProvider,
+                           TemplateRenderer templateRenderer) {
+        this.userManager = userManager;
+        this.loginUriProvider = loginUriProvider;
+        this.templateRenderer = templateRenderer;
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserKey userKey = userManager.getRemoteUserKey(req);
+        if(userKey == null || !userManager.isSystemAdmin(userKey)) { //does the second check need to be made?
+            redirectToLogin(req, resp);
+            return;
+        }
+        templateRenderer.render("epic.vm", resp.getWriter());
+    }
+
+    private void redirectToLogin(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect(loginUriProvider.getLoginUri(getUri(req)).toASCIIString());
+    }
+
+    private URI getUri(HttpServletRequest req) {
+        StringBuffer builder = req.getRequestURL();
+        if(req.getQueryString() != null) {
+            builder.append("?");
+            builder.append(req.getQueryString());
+        }
+        return URI.create(builder.toString());
+    }
+
 }

170a0e516d4426edad6c5a4d4a6add3be9722f10
Jun Fan <fanju@cobalt.com>
2014-07-08 17:47:50 +0000

    add a testcase to test all projects are checked initially.

--- src/test/java/ut/com/cobalt/jira/plugin/epic/test/EpicPluginSpec.js
+++ src/test/java/ut/com/cobalt/jira/plugin/epic/test/EpicPluginSpec.js
@@ -17,11 +17,13 @@ describe('ProjectController', function(){
 
 		//httpBackend.when("GET", "jira/rest/epic/1/projects.json").respond([
         httpBackend.when("GET", "undefined/rest/epic/1/projects.json?days=14").respond([
-        {"name":"newpro","key":"NEW","id":10102,"description":"","epics":[]},
-        {"name":"Project1","key":"PROJ","id":10000,"description":"","epics":[]},
-        {"name":"Project2","key":"TEST","id":10100,"description":"","epics":[]},
-        {"name":"Scrum1","key":"TESTSCRUM","id":10101,"description":"","epics":[
-        {"description":"To test project containing epic","key":"TESTSCRUM-1","id":10000,"name":"Epic1"}]}]);
+        {"name":"newpro","key":"NEW","id":10102,"description":"","timestamp":"1404411614000","epics":[]},
+        {"name":"Project1","key":"PROJ","id":10000,"description":"","timestamp":"1404408360000","epics":[]},
+        {"name":"Project2","key":"TEST","id":10100,"description":"","timestamp":"1404410614000","epics":[]},
+        {"name":"Scrum1","key":"TESTSCRUM","id":10101,"description":"","timestamp":"1404421614000","epics":[
+        {"description":"To test project containing epic","key":"TESTSCRUM-1","id":10000,"name":"Epic1",
+        "timestamp":"1404421614000","stories":[{"name":"issue1","key":"TESTSCRUM-2","id":10100,
+        "timestamp":"1404421614000","sub-tasks":[]}]}]}]);
                       
         //declare the controller and inject our empty scope
         $controller('ProjectController', {
@@ -41,6 +43,12 @@ describe('ProjectController', function(){
     	expect(scope.filter).toBeFalsy();
     });
     
+    
+    it('should initially includes all projects', function(){
+    	angular.forEach(scope.projects, function (project) {
+    		expect(scope.project.included).toBeTrulthy();
+    	});
+    });
     
     
     it('should make a http GET request for projects and get 4 projects', function () {
@@ -62,15 +70,17 @@ describe('ProjectController', function(){
         expect(project1.name).toEqual("newpro");
         expect(project1.key).toEqual("NEW");
         expect(project1.id).toEqual(10102);
-       
+       	expect(project1.timestamp).toEqual("1404411614000");
         project1 = scope.projects[1];
         expect(project1.name).toEqual("Project1");
         expect(project1.key).toEqual("PROJ");
         expect(project1.id).toEqual(10000);
+        expect(project1.timestamp).toEqual("1404408360000");
         project1 = scope.projects[2];
         expect(project1.name).toEqual("Project2");
         expect(project1.key).toEqual("TEST");
         expect(project1.id).toEqual(10100);
+        expect(project1.timestamp).toEqual("1404410614000");
    });
    
    it('should have a project with epic', function(){
@@ -79,11 +89,18 @@ describe('ProjectController', function(){
         expect(project2.name).toEqual("Scrum1");
         expect(project2.key).toEqual("TESTSCRUM");
         expect(project2.id).toEqual(10101);
+        expect(project2.timestamp).toEqual("1404421614000");
         var epic1 = project2.epics[0];
         expect(epic1.name).toEqual("Epic1");
         expect(epic1.key).toEqual("TESTSCRUM-1");
         expect(epic1.id).toEqual(10000);
         expect(epic1.description).toEqual("To test project containing epic");
+        expect(epic1.timestamp).toEqual("1404421614000");
+        var story1 = epic1.stories[0];
+        expect(story1.name).toEqual("issue1");
+        expect(story1.key).toEqual("TESTSCRUM-2");
+        expect(story1.id).toEqual(10100);
+        expect(story1.timestamp).toEqual("1404421614000");
     });
     
     

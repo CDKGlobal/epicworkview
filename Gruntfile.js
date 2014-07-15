ae2ce782fa0d9d7ad55b40f3eae8ba503532d984
Goldman-Kirst <goldmae@cobalt.com>
2014-07-15 13:50:27 -0700

    set up grunt to run unit tests

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    //Interface with the Karma test runner
+    //See: https://github.com/karma-runner/grunt-karma
+    karma: {
+        angularunit: {
+            configFile: 'src/test/resources/karma.config.js',
+            colors: false,
+            singleRun: true
+        }
+    },
+  
+    //protractor - run acceptance tests
+	// See: https://github.com/teerapap/grunt-protractor-runner
+    protractor: {
+    	options: {
+    		keepAlive: false, //if this is true then our builds won't fail on errors
+    		configFile: "src/test/resources/protractor_conf.js",
+    		noColor: false, // If true, protractor will not use colors in its output.
+    		args: {
+    			// Arguments passed to the command
+    		}
+    	}
+    }
+  
+  });
+
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-protractor-runner');
+
+  grunt.registerTask('default', []);
+  grunt.registerTask('ut', ['karma']);
+  grunt.registerTask('it', ['protractor']);
+
+};

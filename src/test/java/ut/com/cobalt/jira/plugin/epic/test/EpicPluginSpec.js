37cc22bca27c8bed419375ab2cca9698b7f478bb
Jun Fan <fanju@cobalt.com>
2014-07-16 20:02:50 +0000

    Modify existing testcases and add more testcases

--- src/test/java/ut/com/cobalt/jira/plugin/epic/test/EpicPluginSpec.js
+++ src/test/java/ut/com/cobalt/jira/plugin/epic/test/EpicPluginSpec.js
@@ -1,7 +1,7 @@
 describe('ProjectController', function(){
     var scope;//we'll use this scope in our tests
     var httpBackend, http, controller, cookieStore;
-    var projects, alphabeticalProjects;
+    var projects, alphabeticalProjects, project1;
     
     var app = angular.module("EpicPlugin", ['ngCookies']);
  
@@ -16,17 +16,37 @@ describe('ProjectController', function(){
         http = $http;
         cookieStore = $cookieStore;
 
-		//httpBackend.when("GET", "jira/rest/epic/1/projects.json").respond([
-        httpBackend.when("GET", "undefined/rest/epic/1/projects.json?seconds=1209600").respond([
-        {"name":"newpro","key":"NEW","id":10102,"description":"","timestamp":"1405438117393","epics":[]},
-        {"name":"Project1","key":"PROJ","id":10000,"description":"","timestamp":"1405438235000","epics":[]},
-        {"name":"Project2","key":"TEST","id":10100,"description":"","timestamp":"1405430614000","epics":[]},
-        {"name":"Project3","key":"NOUPDATE","id":10400,"description":"","timestamp":"1402410614000","epics":[]},
-        {"name":"Scrum1","key":"TESTSCRUM","id":10101,"description":"","timestamp":"1405431614000","epics":[
+		httpBackend.when("GET", "undefined/rest/epic/1/projects.json?seconds=1209600").respond([
+        {"name":"Project1","key":"PROJ","id":10000,"description":"","timestamp":1402410614000,"epics":[
+        {"description":"To test project without change","key":"TESTPRO-1","id":10000,"name":"Myepic",
+        "timestamp":1402410614000,"contributor":{"id":"admin","name":"admin",
+        "avatar":"/jira/secure/useravatar?avatarId=10122"},"stories":[{"name":"issue1","key":"TEST-2",
+        "id":10100, "timestamp":1402410614567,"contributor":{"id":"admin","name":"admin",
+        "avatar":"/jira/secure/useravatar?avatarId=10122"},"completed":true,"sub-tasks":[]}]}]},
+        {"name":"Project2","key":"TEST","id":10100,"description":"","timestamp":1405430614000,"epics":[
+        {"description":"To test project containing older timestamp","key":"TEST-1","id":10100,"name":"TestEpic",
+        "timestamp":1405430614000,"stories":[{"name":"newissue","key":"TEST-2",
+        "id":10100, "timestamp":1405430614987,"completed":false,"sub-tasks":[]}]}]},    
+        {"name":"Project4","description":"","key":"WIRED","id":10400,"timestamp":1405458785061,
+        "epics":[{"name":"epic for today","description":"eipc for today test","key":"WIRED-1",
+        "id":10400,"timestamp":1405458785061,"contributor":{"id":"admin","name":"admin",
+        "avatar":"/jira/secure/useravatar?avatarId=10122"},"stories":[{"name":"story1",
+        "key":"WIRED-2","id":10401,"timestamp":1405457509149,"contributor":{"id":"admin",
+        "name":"admin","avatar":"/jira/secure/useravatar?avatarId=10122"},"completed":true,
+        "subtasks":[]},{"name":"story2","key":"WIRED-3","id":10402,"timestamp":1405438117393,
+        "contributor":{"id":"admin","name":"admin","avatar":"/jira/secure/useravatar?avatarId=10122"},
+        "completed":false,"subtasks":[]},{"name":"story3","key":"WIRED-4","id":10403,"timestamp":1405458785061,
+        "contributor":{"id":"admin","name":"admin","avatar":"/jira/secure/useravatar?avatarId=10122"},
+        "completed":true,"subtasks":[]}]}]},
+        {"name":"Scrum1","key":"TESTSCRUM","id":10101,"description":"","timestamp":1405431614000,"epics":[
+        {"name":"Other Stories","description":"Stories without an epic","key":"NOKEY","id":-1,"timestamp":1405430232445,
+        "stories":[{"name":"test 2","key":"ESCRUM-33","id":186652,"timestamp":1405430232445,
+        "contributor":{"id":"emgej","name":"Emge, Justin","avatar":"/secure/useravatar?avatarId=10102"},
+        "completed":false,"subtasks":[]}]},
         {"description":"To test project containing epic","key":"TESTSCRUM-1","id":10000,"name":"Epic1",
-        "timestamp":"1405431614000","contributor":{"id":"admin","name":"admin",
+        "timestamp":1405431614000,"contributor":{"id":"admin","name":"admin",
         "avatar":"/jira/secure/useravatar?avatarId=10122"},"stories":[{"name":"issue1","key":"TESTSCRUM-2",
-        "id":10100, "timestamp":"1405431614000","contributor":{"id":"admin","name":"admin",
+        "id":10100, "timestamp":1405431614000,"contributor":{"id":"admin","name":"admin",
         "avatar":"/jira/secure/useravatar?avatarId=10122"},"completed":false,"sub-tasks":[]}]}]}]);
                       
         //declare the controller and inject our empty scope
@@ -35,7 +55,6 @@ describe('ProjectController', function(){
                   $http: http,
                   $cookieStore: cookieStore
               });
-        //$controller('ProjectController', {$scope: scope});
     }));
     
     
@@ -56,74 +75,136 @@ describe('ProjectController', function(){
     });
     
     
-    // test the Project3 which has not been updated in 14 days will not shown in project list
-    // only 4 projects will be returned
-    it('should make a http GET request for projects and get 4 projects', function () {
-        httpBackend.expectGET('undefined/rest/epic/1/projects.json?seconds=1209600');
-        
+    // test the Project1 which has not been updated in 14 days will not shown in project list
+    // only 3 projects will be returned
+    it('should make a http GET request for projects and get 3 projects', function () {
+        httpBackend.expectGET('undefined/rest/epic/1/projects.json?seconds=1209600');        
         httpBackend.flush();
         expect(scope.projects).not.toBeNull();
-        expect(scope.projects.length).toEqual(4);
+        expect(scope.projects.length).toEqual(3);
     });
     
-            
+        
+    // test the returned projects are time sorted    
     it('should sort the projects based on the timestamp', function(){    
     	httpBackend.flush();
         expect(scope.projects[0]).not.toBeNull();
-        var project1 = scope.projects[0];
-        // Project1 is the newest updated project
-        expect(project1.name).toEqual("Project1");
-        expect(project1.key).toEqual("PROJ");
-        expect(project1.id).toEqual(10000);
-       	expect(project1.timestamp).toEqual("1405438235000");
+        project1 = scope.projects[0];
+        // Test "Project4" is the newest updated project
+        expect(project1.name).toEqual("Project4");
+        expect(project1.key).toEqual("WIRED");
+        expect(project1.id).toEqual(10400);
+       	expect(project1.timestamp).toEqual(1405458785061);
         project1 = scope.projects[1];
-        expect(project1.name).toEqual("newpro");
-        expect(project1.key).toEqual("NEW");
-        expect(project1.id).toEqual(10102);
-        expect(project1.timestamp).toEqual("1405438117393");
-        project1 = scope.projects[2];
         expect(project1.name).toEqual("Scrum1");
         expect(project1.key).toEqual("TESTSCRUM");
         expect(project1.id).toEqual(10101);
-        expect(project1.timestamp).toEqual("1405431614000");
-        project1 = scope.projects[3];
+        expect(project1.timestamp).toEqual(1405431614000);
+        project1 = scope.projects[2];
         expect(project1.name).toEqual("Project2");
         expect(project1.key).toEqual("TEST");
         expect(project1.id).toEqual(10100);
-        expect(project1.timestamp).toEqual("1405430614000");
+        expect(project1.timestamp).toEqual(1405430614000);
    });
    
         
    it('should have a project with epic, story and contributor', function(){
    		httpBackend.flush();
-        var project2 = scope.projects[2];
-        expect(project2.name).toEqual("Scrum1");
-        expect(project2.key).toEqual("TESTSCRUM");
-        expect(project2.id).toEqual(10101);
-        expect(project2.timestamp).toEqual("1405431614000");
-        var epic1 = project2.epics[0];
+        project1 = scope.projects[1];
+        expect(project1.name).toEqual("Scrum1");
+        expect(project1.key).toEqual("TESTSCRUM");
+        expect(project1.id).toEqual(10101);
+        expect(project1.timestamp).toEqual(1405431614000);
+        var epic1 = project1.epics[1];
         expect(epic1.name).toEqual("Epic1");
         expect(epic1.key).toEqual("TESTSCRUM-1");
         expect(epic1.id).toEqual(10000);
         expect(epic1.description).toEqual("To test project containing epic");
-        expect(epic1.timestamp).toEqual("1405431614000");
+        expect(epic1.timestamp).toEqual(1405431614000);
         var story1 = epic1.stories[0];
         expect(story1.name).toEqual("issue1");
         expect(story1.key).toEqual("TESTSCRUM-2");
         expect(story1.id).toEqual(10100);
-        expect(story1.timestamp).toEqual("1405431614000");
+        expect(story1.timestamp).toEqual(1405431614000);
         expect(story1.completed).toBe(false);
         expect(story1.contributor.id).toEqual("admin");
         expect(story1.contributor.name).toEqual("admin");
         expect(story1.contributor.avatar).toEqual("/jira/secure/useravatar?avatarId=10122");
     });
     
+    // test "Project4" has 2 completed stories
+    it('should have a project with 2 completed stories', function(){
+   		httpBackend.flush();
+        project1 = scope.projects[0];
+        expect(project1.name).toEqual("Project4");
+        expect(scope.getCompletedStories(project1)).toEqual(2);            
+    });
     
+    // test "Scrum1" has no completed stories
+    it('should have a project named Scrum1 with 0 completed stories', function(){
+   		httpBackend.flush();
+        project1 = scope.projects[1];
+        expect(project1.name).toEqual("Scrum1");
+        expect(scope.getCompletedStories(project1)).toEqual(0);            
+    });
+    
+    // test "Project2" has no contributor
+    it('should have a project named Project2 with no contributor', function(){
+   		httpBackend.flush();
+        project1 = scope.projects[2];
+        expect(project1.name).toEqual("Project2");
+        expect(project1.id).toEqual(10100);
+        expect(scope.getContributors(project1).length).toEqual(0);            
+    });
+    
+    // test all the stories updated in "Project4" are done by 1 contributor
+    it('should have a project named Project4 with 1 contributor', function(){
+   		httpBackend.flush();
+        project1 = scope.projects[0];
+        expect(scope.getContributors(project1).length).toEqual(1);            
+    });
+    
+    // test the stories updated in "Scrum1" are done by different contributors
+    it('should have a project named Scrum1 with more than 1 contributor', function(){
+   		httpBackend.flush();
+        project1 = scope.projects[1];
+        expect(project1.name).toEqual("Scrum1");
+        expect(scope.getContributors(project1).length).toEqual(2);            
+    });
+    
+    
+    // test the alphabeticalProjects are alphabetically sorted
+    it('should have alphabeticalProjects alphabetically sorted', function(){
+   		httpBackend.flush();
+        expect(scope.alphabeticalProjects().length).toEqual(3); 
+        project1 = scope.alphabeticalProjects()[0]; 
+        expect(project1.name).toEqual("Project2");
+        project1 = scope.alphabeticalProjects()[1]; 
+        expect(project1.name).toEqual("Project4");
+        project1 = scope.alphabeticalProjects()[2]; 
+        expect(project1.name).toEqual("Scrum1");
+        
+    });
+    
+    
+    // test the timeOrderedProjects are sorted by time order
+    it('should have timeOrderedProjects sorted by time order', function(){
+   		httpBackend.flush();
+        expect(scope.timeOrderedProjects().length).toEqual(3); 
+        project1 = scope.timeOrderedProjects()[0]; 
+        expect(project1.name).toEqual("Project4");
+        project1 = scope.timeOrderedProjects()[1]; 
+        expect(project1.name).toEqual("Scrum1");
+        project1 = scope.timeOrderedProjects()[2]; 
+        expect(project1.name).toEqual("Project2");
+        
+    });
+       		
     
 });
 
 
-xdescribe('EpicController', function(){
+describe('EpicController', function(){
     var scope;//we'll use this scope in our tests
     var controller;
  

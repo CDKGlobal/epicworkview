664a1ca9ba5370ad78cee58ba0513474ff254912
Goldman-Kirst <goldmae@cobalt.com>
2014-08-01 09:08:38 -0700

    added progress bar

--- src/main/resources/js/EpicDetails.js
+++ src/main/resources/js/EpicDetails.js
@@ -1,8 +1,14 @@
 function epicDetailsController ($scope, $http, $q, $location) {
+	var notStartedNames = ["To Do", "Open"];
+	var doneNames = ["Closed", "Resolved", "Done"];
+	
     $scope.contextPath = jQuery('meta[name="ajs-context-path"]').attr('content');
     $scope.epicKey = $location.search().epic;
     $scope.epicName = '';
     $scope.stories = [];
+    $scope.notStarted = 0;
+    $scope.inProgress = 0;
+    $scope.done = 0;
 
     $scope.workType = 1;
 
@@ -32,10 +38,24 @@ function epicDetailsController ($scope, $http, $q, $location) {
         $scope.epicName = getField(epic.fields, 'Epic Name');
 
         $scope.stories = stories.issues;
+        countStories($scope.stories);
     });
 
     function getField(data, field) {
         return data[fieldMap[field]];
+    }
+    
+    // count the number of not started, in progress, and done stories
+    function countStories(stories) {
+    	angular.forEach(stories, function(story, index) {
+    		if (jQuery.inArray(story.fields.status.name, notStartedNames) != -1) {
+    			$scope.notStarted++;
+    		} else if (jQuery.inArray(story.fields.status.name, doneNames) != -1) {
+    			$scope.done++;
+    		} else {
+    			$scope.inProgress++;
+    		}
+    	});
     }
 }
 

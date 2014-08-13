9865697281056a8dbc9fc29ed6e4df92f84e196c
Justin Emge <emgej@cobalt.com>
2014-08-13 15:59:39 +0000

    Fixed sonar issues

--- src/main/resources/js/EpicDetails.js
+++ src/main/resources/js/EpicDetails.js
@@ -569,40 +569,39 @@ function chartDirective() {
                 chart.setSelection(ranges);
             });
             
-             // watch for hover            
+            // watch for hover            
      
             var previousPoint = null;
-            jQuery("#chart").bind("plothover", function (event, pos, item) {            	
-            	if (item && ((item.seriesIndex == 2) || (item.seriesIndex == 3))){
-            		if (previousPoint !== item.dataIndex) {
-                		previousPoint = item.dataIndex;
-                		jQuery("#tooltip").remove();
-						var x = item.datapoint[0];
-							y = item.datapoint[1];
-						var index = item.dataIndex;
-						console.log(item.series.data[index]);
-						console.log(index);	
-						if (index !== 0){
-							var previousx = item.series.data[index - 1][0];
-							if (item.seriesIndex == 2){
-								showTooltip(item.pageX, item.pageY,
-                  				"From " + new Date(previousx).toLocaleDateString() + " to " + new Date(x).toLocaleDateString() + "<br/>" + "<strong>" + y + "</strong>" + " " + scope.workTypeToString() +" created");
-                  			}else{
-                  				showTooltip(item.pageX, item.pageY,
-                  				"From " + new Date(previousx).toLocaleDateString() + " to " + new Date(x).toLocaleDateString() + "<br/>" + "<strong>" + y + "</strong>" + " " + scope.workTypeToString() +" Resolved");
-                  			}
-                  			
-						}else{	
-							if (item.seriesIndex == 2){	
-								showTooltip(item.pageX, item.pageY,
-                  				new Date(x).toLocaleDateString() + "<br/>" + "<strong>" + y + "</strong>" + " " + scope.workTypeToString() + " created");
-                  			}else{
-                  				showTooltip(item.pageX, item.pageY,
-                  				new Date(x).toLocaleDateString() + "<br/>" + "<strong>" + y + "</strong>" + " " + scope.workTypeToString() + " resolved");
-                  			}
-          
-                  		}
-                  	}
+            jQuery("#chart").bind("plothover", function (event, pos, item) {
+            	if (item && item.seriesIndex > 1){
+            	    if(previousPoint !== item.dataIndex) {
+                        previousPoint = item.dataIndex;
+
+                	    jQuery("#tooltip").remove();
+
+					    var x = item.datapoint[0];
+					    var y = item.datapoint[1];
+
+					    var index = item.dataIndex;
+					    var msg = '';
+
+					    if (index !== 0) {
+						    var previousx = item.series.data[index - 1][0];
+						    msg += "From " + new Date(previousx).toLocaleDateString() + " to ";
+					    }
+
+                  	    msg += new Date(x).toLocaleDateString() + "<br/><strong>" + y + "</strong>";
+
+                  	    if (item.seriesIndex === 2){
+                            msg += " Created";
+                        }
+                        else{
+                            msg += " Resolved";
+                        }
+
+
+                        showTooltip(item.pageX, item.pageY, msg);
+                    }
 				} else {
 					previousPoint = null;
 					jQuery("#tooltip").hide();

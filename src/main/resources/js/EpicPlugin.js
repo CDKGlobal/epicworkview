2afccc6fbe3e6ca6c9384af51f778dcfffec6b04
Justin Emge <emgej@cobalt.com>
2014-08-15 20:29:52 +0000

    removed unused directives and cleaned up the EpicPlugin.js

--- src/main/resources/js/EpicPlugin.js
+++ src/main/resources/js/EpicPlugin.js
@@ -1,23 +1,12 @@
+//initialize our module with ngAnimate, ngCookies and ui.bootstrap as dependencies
 angular.module('WorkView', ['ngAnimate', 'ngCookies', 'ui.bootstrap']);
 
+//enable html5 mode so that $location.search() works as expected
 angular.module('WorkView').config(function($locationProvider) {
     $locationProvider.html5Mode(true);
 });
 
-angular.module('WorkView').constant('Context', jQuery('meta[name="ajs-context-path"]').attr('content'));
-
-angular.module('WorkView').controller('testController', ['$rootScope', '$scope', 'ProjectsFactory', function($rootScope, $scope, projectsFactory) {
-    $scope.log = function() {
-        console.log(projectsFactory.getProjects());
-        console.log(projectsFactory.isLoading());
-    };
-    $scope.log();
-
-    $scope.createProject = function() {
-        $rootScope.$broadcast('newProject', {
-            id: 10001,
-            name: 'Test',
-            group: 'no category'
-        });
-    };
-}]);
+//simple factory that returns the current context of the application
+angular.module('WorkView').factory('Context', function() {
+    return jQuery('meta[name="ajs-context-path"]').attr('content');
+});
